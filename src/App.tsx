1207e64a908b5cfdb85ba0a3d621d54ac1389517
Tan Thong <levantanthong.2602@gmail.com>
2024-12-27 14:03:27 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import Header from "./layouts/Header";
+import Banner from "./layouts/Banner";
+import "./App.css"
+import Collection from "./layouts/Collection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <div className="notify">Holiday Sale. Up to 50% OFF — Don't sleep on it.</div>
+      <Header />
+      <Banner />
+      <div className="main">
+          <Collection />
+
+      </div>
     </div>
   );
 }
---
+++ src/layouts/Collection.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+import "./../css/collection.css"
+
+function Collection() {
+    return(
+        <div className="selection">
+            <div className="shop">
+                <p className="title">Explore a selection of our wardrobe essentials.</p>
+                <a href="">Shop all</a>
+            </div>
+            <div className="list-categories">
+                <a href="" className="category">
+                    <img className="image" src={require("./../images/categories/Sneakers.webp")} alt="" />
+                    <div className="infor">
+                        <p className="title"><b>Sneakers</b></p>
+                        <p className="desc">Sneakers made like shoes.</p>
+                    </div>
+                </a>
+
+                <a href="" className="category">
+                    <img className="image" src={require("./../images/categories/Sneakers.webp")} alt="" />
+                    <div className="infor">
+                        <p className="title"><b>Sneakers</b></p>
+                        <p className="desc">Sneakers made like shoes.</p>
+                    </div>
+                </a>
+
+                <a href="" className="category">
+                    <img className="image" src={require("./../images/categories/Sneakers.webp")} alt="" />
+                    <div className="infor">
+                        <p className="title"><b>Sneakers</b></p>
+                        <p className="desc">Sneakers made like shoes.</p>
+                    </div>
+                </a>
+
+                <a href="" className="category">
+                    <img className="image" src={require("./../images/categories/Sneakers.webp")} alt="" />
+                    <div className="infor">
+                        <p className="title"><b>Sneakers</b></p>
+                        <p className="desc">Sneakers made like shoes.</p>
+                    </div>
+                </a>
+
+                <a href="" className="category">
+                    <img className="image" src={require("./../images/categories/Sneakers.webp")} alt="" />
+                    <div className="infor">
+                        <p className="title"><b>Sneakers</b></p>
+                        <p className="desc">Sneakers made like shoes.</p>
+                    </div>
+                </a>
+
+                <a href="" className="category">
+                    <img className="image" src={require("./../images/categories/Sneakers.webp")} alt="" />
+                    <div className="infor">
+                        <p className="title"><b>Sneakers</b></p>
+                        <p className="desc">Sneakers made like shoes.</p>
+                    </div>
+                </a>
+
+                <a href="" className="category">
+                    <img className="image" src={require("./../images/categories/Sneakers.webp")} alt="" />
+                    <div className="infor">
+                        <p className="title"><b>Sneakers</b></p>
+                        <p className="desc">Sneakers made like shoes.</p>
+                    </div>
+                </a>
+
+                <a href="" className="category">
+                    <img className="image" src={require("./../images/categories/Sneakers.webp")} alt="" />
+                    <div className="infor">
+                        <p className="title"><b>Sneakers</b></p>
+                        <p className="desc">Sneakers made like shoes.</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default Collection; 
